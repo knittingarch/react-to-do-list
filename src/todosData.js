a2dd59443f73d0be6c4ecddcc97b0fef3523b5be
Sarah Dawson <knittingarch@gmail.com>
2019-09-27 16:14:46 -0400

    First commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Walk the dog",
+    completed: true,
+  },
+  {
+    id: 2,
+    text: "Get a haircut",
+    completed: false,
+  },
+  {
+    id: 3,
+    text: "Paint my nails",
+    completed: true,
+  },
+  {
+    id: 4,
+    text: "Get groceries",
+    completed: false,
+  },
+  {
+    id: 5,
+    text: "Mail packages",
+    completed: false,
+  },
+]
+
+export default todosData
