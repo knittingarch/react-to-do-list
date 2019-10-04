2baf82266a27f8787321c079949d184a85158129
Sarah Dawson <knittingarch@gmail.com>
2019-10-04 18:04:29 -0400

    Create Basic Components for Quick Create Section
    The components created here are placeholders for more robust design and
    state.  Currently mocking up the components I will need for the MVP and
    then will add state and interactivity as well as design.
    
    The Quick Create section will be the top level way to create a task, but
    users will also be able to click "more" if they want to add a set date or
    frequency.  The defaults will be the current date and non-repeating.

---
+++ src/TaskInput.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function TaskInput() {
+  return (
+    <div>
+      <input type="text" id="task" />
+      <label for="task">Task</label>
+    </div>
+
+  )
+}
+
+export default TaskInput
